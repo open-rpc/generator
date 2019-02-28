ab6531c480bbc858cb121aff677194619d5e4af9
Zachary Belford <belfordz66@gmail.com>
2019-02-27 18:50:16 -0800

    fix: add paramStructure support fixes #22

--- templates/js/templated/exported-class.template.js
+++ templates/js/templated/exported-class.template.js
@@ -1,6 +1,7 @@
 module.exports = require('lodash').template(`
 import * as jayson from "jayson/promise";
 import Djv from "djv";
+import { zipObject } from "lodash";
 
 export default class <%= className %> {
   rpc: jayson.Client;
@@ -26,8 +27,14 @@ export default class <%= className %> {
   <% methods.forEach((method, i) => { %>
   <%= method.name %>(...params: Array<any>): jayson.JSONRPCRequest {
     params.forEach((param, i) => this.validators['<%= method.name %>'][i].validate(param));
-    return this.rpc.request("<%= method.name %>", params);
+    <% if (method.paramStructure && method.paramStructure === "by-name") { %>
+    const rpcParams = params;
+    <% } else { %>
+    const rpcParams = zipObject(params, <%= _.map(method.params, 'name') %>);
+    <% } %>
+    return this.rpc.request("<%= method.name %>", rpcParams);
   }
+
   <% }) %>
 }
 `);
