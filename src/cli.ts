390cd300d78473c5fac362f0fb1a3b0bf1d70a05
Zachary Belford <belfordz66@gmail.com>
2019-05-03 09:58:09 -0700

    fix: remove usage alltogether

--- src/cli.ts
+++ src/cli.ts
@@ -5,7 +5,6 @@ import orpcGenerator from "./";
 import { parseOpenRPCDocument } from "@open-rpc/schema-utils-js";
 
 program
-  .usage("[options]")
   .option("-d, --document [openrpcDocument]", "JSON string or a Path/Url pointing to an open rpc schema", "./openrpc.json")
   .action(async () => {
     const openrpcDocument = await parseOpenRPCDocument(program.document);
