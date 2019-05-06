a044e1de74ea5efc8ef0777778da8d25d3b2f678
Zachary Belford <belfordz66@gmail.com>
2019-05-06 13:33:54 -0700

    fix: linting issue with cli

--- src/cli.ts
+++ src/cli.ts
@@ -4,12 +4,14 @@ import program = require("commander");
 import orpcGenerator from "./";
 import { parseOpenRPCDocument } from "@open-rpc/schema-utils-js";
 
+const version = require("../../package.json").version; // tslint:disable-line
+
 program
-  .version(require('../../package.json').version, '-v, --version')
+  .version(version, "-v, --version")
   .option(
     "-d, --document [openrpcDocument]",
     "JSON string or a Path/Url pointing to an open rpc schema",
-    "./openrpc.json"
+    "./openrpc.json",
   )
   .action(async () => {
     const openrpcDocument = await parseOpenRPCDocument(program.document);
