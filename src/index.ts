9ec71adbf0683094ad05b8824fa01fc585fb9dce
Zachary Belford <belfordz66@gmail.com>
2019-05-14 16:39:35 -0700

    feat(rust): handle complex openrpc documents

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,9 @@ import { ensureDir, emptyDir, copy, move } from "fs-extra";
 import * as path from "path";
 import { promisify } from "util";
 import { map, trim, startCase } from "lodash";
-import { MethodTypings } from "@open-rpc/schema-utils-js";
 import { OpenRPC } from "@open-rpc/meta-schema";
+
+import MethodTypings from "@open-rpc/typings";
 
 import _bootstrapGeneratedPackage from "./bootstrapGeneratedPackage";
 
@@ -20,10 +21,11 @@ const cleanBuildDir = async (destinationDirectoryName: string): Promise<any> => 
   await emptyDir(destinationDirectoryName);
 };
 
-const compileTemplate = async (openrpcDocument: OpenRPC, language: string): Promise<string> => {
-  const methodTypings = new MethodTypings(openrpcDocument);
-  await methodTypings.generateTypings();
-
+const compileTemplate = async (
+  openrpcDocument: OpenRPC,
+  language: string,
+  methodTypings: MethodTypings
+): Promise<string> => {
   const template = language === "rust" ? rsTemplate : jsTemplate;
   return template({
     className: startCase(openrpcDocument.info.title).replace(/\s/g, ""),
@@ -60,11 +62,14 @@ const languageFilenameMap: any = {
   typescript: "index.ts",
 };
 
-export default (generatorOptions: IGeneratorOptions) => {
+export default async (generatorOptions: IGeneratorOptions) => {
   const { openrpcDocument, outDir } = generatorOptions;
 
+  const methodTypings = new MethodTypings(openrpcDocument);
+  await methodTypings.generateTypings();
+
   return Promise.all(["typescript", "rust"].map(async (language) => {
-    const compiledResult = await compileTemplate(openrpcDocument, language);
+    const compiledResult = await compileTemplate(openrpcDocument, language, methodTypings);
 
     const destinationDirectoryName = `${outDir}/${language}`;
     await cleanBuildDir(destinationDirectoryName);
