d7f749e6dfced6e762e2405f8b17dc37370c35b6
Javad <ja7ad@live.com>
2025-05-03 15:03:00 +0330

    fix: add normalization dot for method name

---
+++ src/components/helper.ts
@@ -0,0 +1,11 @@
+export function normalizeMethodName(name: string): string {
+  // backward compatibility only change on dot inside in name.
+  if (!name.includes('.')) return name;
+
+  const parts = name.split(/[\._]/);
+  return parts
+    .map((part, index) =>
+      index === 0 ? part : part.charAt(0).toUpperCase() + part.slice(1)
+    )
+    .join('');
+}
